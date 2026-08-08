b99675986e0d2b985b922dc90d3a0e5e0e6dc0ea
Daraja <77206690+zayqu@users.noreply.github.com>
2026-08-09 00:38:52 +0300

    Restore src/App.jsx to pre-responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -50,9 +50,9 @@ const ScrollToTop = () => {
 };
 
 const PublicLayout = () => (
-  <div className="public-mobile-shell flex min-h-screen min-w-0 max-w-full flex-col overflow-x-clip">
+  <div className="public-mobile-shell flex min-h-screen flex-col">
     <Header />
-    <main className="min-w-0 flex-grow"><Outlet /></main>
+    <main className="flex-grow"><Outlet /></main>
     <Footer />
     <MobileBottomNav />
   </div>
